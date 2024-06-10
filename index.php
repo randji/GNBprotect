3e708b3f8385855b74e754063ed285776a32addb
randji <stephanesoilihi@yahoo.fr>
2024-06-10 11:20:01 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>GNB-PROTECT</title>
+</head>
+<body>
+    <header>
+        <p class="entreprise">GNB-PROTECT</p>
+    </header>
+
+    <div class="banniere">
+        <img class="imgbanniere" src="img/photbanniere.png" alt="logo">
+    </div>
+    <main>
+    <div class="title">
+        <p class="slogan">GNB-PROTECT <br> votre partenaire de confiance pour l'installation de matériel de surveillance</p>
+    </div>
+    <div class="detail">
+        <button class="contact"> contactez nous</button>
+        <p>Notre entreprise se distingue par son professionnalisme, sa rapidité d'intervention et la qualité exceptionnelle de ses équipements.
+        <br>
+        <br>
+        Notre équipe d'experts qualifiés s'engage à fournir des solutions sur mesure, adaptées à vos besoins spécifiques, en garantissant une installation rapide et efficace. Chez Paris Surveillance, nous utilisons uniquement des produits de haute qualité pour assurer la sécurité optimale de vos locaux.
+        <br>
+        <br>
+        Faites confiance à Paris Surveillance pour une surveillance fiable et une tranquillité d'esprit totale.</p>
+
+        <p>GALLERY PHOTO</p>
+    </div>
+    </main>
+    
+    <footer>
+        <p class="footertitle">gnp-protect</p>
+        
+    </footer>
+</body>
+</html>

02ff34e58df1dd146c1657844dd395b813cf046a
randji <stephanesoilihi@yahoo.fr>
2024-06-19 23:33:24 +0200

    caroussel et import bootstrap

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,21 @@
-const slide = ["img/IMG-20240605-WA0010.png", "img/IMG-20240605-WA0020.png", "img/IMG-20240605-WA0023.png", "img/IMG-20240605-WA0030.png"];
+// const slide = ["img/IMG-20240605-WA0010.png", "img/IMG-20240605-WA0020.png", "img/IMG-20240605-WA0023.png", "img/IMG-20240605-WA0030.png"];
 
-let numero = 0;
+// let numero = 0;
 
-function ChangeSlide(sens) {
-    numero = numero + sens;
-    if (numero < 0)
-        numero = slide.length - 1;
-    if (numero > slide.length - 1)
-        numero = 0;
-    let imgElement = document.getElementById("slide");
-    imgElement.classList.add("fade");
-    setTimeout(() => {
-        imgElement.src = slide[numero];
-        setTimeout(() => {
-            imgElement.classList.remove("fade");
-        }, 50);
-    }, 1000);
-}
+// function ChangeSlide(sens) {
+//     numero = numero + sens;
+//     if (numero < 0)
+//         numero = slide.length - 1;
+//     if (numero > slide.length - 1)
+//         numero = 0;
+//     let imgElement = document.getElementById("slide");
+//     imgElement.classList.add("fade");
+//     setTimeout(() => {
+//         imgElement.src = slide[numero];
+//         setTimeout(() => {
+//             imgElement.classList.remove("fade");
+//         }, 100);
+//     }, 1000);
+// }
 
-setInterval("ChangeSlide(1)", 4000);
+// setInterval("ChangeSlide(1)", 4000);
--- index.php
+++ index.php
@@ -9,7 +9,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom.css">
     <title>GNB-PROTECT</title>
 </head>
 <body>
@@ -72,13 +75,30 @@
         <button class="contact"> CONTACTEZ NOUS</button>
     </div>
 
-    <div class="gallery">
-        <p>gallery</p>
-        <div class="slider" id="slider">
-            <img class="imgSlider" src="img/IMG-20240605-WA0010.png" alt="" id="slide">
-            <div id="precedent" onclick="ChangeSlide(-1)"><</div>
-            <div id="suivant" onclick="ChangeSlide(1)">></div>
+    <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="img/IMG-20240605-WA0008.jpg" alt="">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/IMG-20240605-WA0010.png" alt="">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/IMG-20240605-WA0030.png" alt="">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/IMG-20240605-WA0020.png" alt="">
+            </div>
         </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="carouselExampleAutoplaying"   data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
     </div>
 
     <h2>Contactez-nous</h2>
@@ -105,6 +125,8 @@
         <p class="footertitle">gnp-protect</p>
         
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="JS/script.js"></script>
 </body>
 </html>
